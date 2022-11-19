7d5ffe46aefefc12c6bd984407595721190181e7
ariful4082 <ariful4082@gmail.com>
2022-11-19 21:30:25 +0600

    fixed Number.toFixed issue

--- components/Checkout/OrderSummary.js
+++ components/Checkout/OrderSummary.js
@@ -8,6 +8,12 @@ export default function OrderSummary({data}) {
   const price = data?.reduce((prev,cur)=> prev+ cur.price, 0);
   const shipping = (price/100)*10;
   const total = price+shipping;
+
+  const handleTotalAmount =()=>{
+    if(total){
+      router.push('/checkout')
+    }
+  }
   return (
     <Box1>
       <Heading3 border variant="h3">
@@ -16,17 +22,17 @@ export default function OrderSummary({data}) {
 
       <Stack direction="row" justifyContent="space-between">
         <Typography variant="body2">Subtotal</Typography>
-        <Typography variant="body1">${price.toFixed(2)}</Typography>
+        <Typography variant="body1">${parseFloat(price).toFixed(2)}</Typography>
       </Stack>
       <Stack direction="row" justifyContent="space-between">
         <Typography variant="body2">Shipping 10%</Typography>
-        <Heading3>${shipping.toFixed(2)}</Heading3>
+        <Heading3>${parseFloat(shipping).toFixed(2)}</Heading3>
       </Stack>
       <Stack direction="row" justifyContent="space-between">
         <Typography variant="body2">Total</Typography>
-        <Typography variant="body1"><Heading3>${total.toFixed(2)}</Heading3></Typography>
+        <Typography variant="body1"><Heading3>${parseFloat(total).toFixed(2)}</Heading3></Typography>
       </Stack>
-      <ButtonMaster fullWidth onClick={() => router.push("/checkout")}>
+      <ButtonMaster fullWidth onClick={handleTotalAmount}>
         Complete checkout
       </ButtonMaster>
     </Box1>
